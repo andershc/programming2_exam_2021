0ae6a0ef3c93b9415d67a077a97724c260823a08
Anders Heftøy Carlsen <andershc@stud.ntnu.no>
2021-05-10 11:38:55 +0200

    fxml, and created controller

---
+++ src/main/java/org/ntnu/andershc/miniproject/controllers/MainController.java
@@ -0,0 +1,20 @@
+package org.ntnu.andershc.miniproject.controllers;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
+public class MainController {
+
+    public MenuItem importCSV;
+    public MenuItem exportCSV;
+    public MenuItem exit;
+    public TextField searchField;
+    public TableColumn postalCodeColumn;
+    public TableColumn postalNameColumn;
+    public TableColumn municipalCodeColumn;
+    public TableColumn municipalNameColumn;
+    public TableColumn categoryColumn;
+    public Label status;
+}
