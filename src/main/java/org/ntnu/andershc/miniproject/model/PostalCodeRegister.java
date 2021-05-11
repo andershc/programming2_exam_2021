253efd7bdc72f57c49b2f0a2d8fa2d01277598bd
Anders Heftøy Carlsen <andershc@stud.ntnu.no>
2021-05-11 12:18:38 +0200

    Added a try catch in the addPostalCode method

--- src/main/java/org/ntnu/andershc/miniproject/model/PostalCodeRegister.java
+++ src/main/java/org/ntnu/andershc/miniproject/model/PostalCodeRegister.java
@@ -16,28 +16,27 @@ public class PostalCodeRegister {
 
     /**
      * Method for adding a postal code to the register
-     * @param postalCode
-     * @param postalName
-     * @param municipalCode
-     * @param municipalName
-     * @param category
      * @throws AddException if the postal code already exists in the register
      */
     public void addPostalCode(String postalCode, String postalName,
                            String municipalCode, String municipalName,
                            String category) throws AddException {
-        if(!postalCodes.containsKey(postalCode)){
-            postalCodes.put(postalCode,
-                    new PostalCode(postalCode, postalName, municipalCode, municipalName, category.charAt(0)));
+        try{
+            PostalCode newPostalCode = new PostalCode(postalCode, postalName, municipalCode, municipalName, category.charAt(0));
+            if(!postalCodes.containsKey(postalCode)){
+                postalCodes.put(postalCode, newPostalCode);
+            }
+            else{
+                throw new AddException(postalCode + " is already registered.");
+            }
+        } catch(IllegalArgumentException | NullPointerException e){
+            System.out.println(e.getMessage());
         }
-        else{
-            throw new AddException(postalCode + " is already registered.");
-        }
+
     }
 
     /**
      * Method that returns all postal codes in the register.
-     * @return
      */
     public HashMap<String, PostalCode> getAllPostalCodes(){
         HashMap<String, PostalCode> allPostalCodes = new HashMap<>();
